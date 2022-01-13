2ef601c37b8d2dd596e8f12f4c1d9718deb7a652
dootrex <avigill@hotmail.ca>
2022-01-12 19:32:25 -0800

    saying hello

--- server.js
+++ server.js
@@ -5,5 +5,9 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "build")));
 
+app.get("/hello", (req, res) => {
+  res.send("hello");
+});
+
 const port = 8080;
 app.listen(port, () => console.log("listening on port 8080"));
